ca4d14b1b690c909b6f46615bbc6156b37a8afe2
anandramappuswamy <anandram.appuswamy@gmail.com>
2021-07-12 10:10:48 +0100

    added validators for name and capital

--- paymentsense-coding-challenge-website/src/app/components/addcountries/addcountry/addcountry.component.ts
+++ paymentsense-coding-challenge-website/src/app/components/addcountries/addcountry/addcountry.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Country } from 'src/app/interfaces/country';
 import { CountriesService } from 'src/app/services/country.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { Currency } from 'src/app/interfaces/currency';
 
@@ -17,21 +17,22 @@ export class AddcountryComponent implements OnInit {
   }
 
   countryForm = new FormGroup({
-    countryName: new FormControl(''),
-    capital: new FormControl(''),
+    countryName: new FormControl('', Validators.required),
+    capital: new FormControl('', Validators.required),
   });
 
   ngOnInit() {
   }
 
   onSubmit() {
+    // TODO: No time to design the UI, so added dummy values for other props. 
     var country = new Country();
     country.name = this.countryForm.value.countryName;
     country.capital = this.countryForm.value.capital;
     country.flag = "https://restcountries.eu/data/ggy.svg";
     country.population = 200000;
     country.timezones = ["UTC+06:00"];
-    country.borders = ["CHN", "IND"];
+    country.borders = ["CHN", "IND"];    
     this.countriesService.addCountry(country).subscribe((data) => {
       this.matSnackBar.open(this.countryForm.value.countryName + " added successfully.", '', { duration: 3000, verticalPosition: 'top' })
     });
